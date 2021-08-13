b50accaf83dadc71a79e0db1296d00470721ae5d
KaMeHb-UA <marlock@bassteam.ml>
2021-08-13 08:39:17 +0300

    default rate

--- src/controllers/currencyRate/monobank.js
+++ src/controllers/currencyRate/monobank.js
@@ -1,8 +1,14 @@
 import request from '../../providers/monobank.js';
 import { UAH, USD } from '../../providers/iso4217.js';
 
+const defaultRate = 27;
+
 export default async () => {
-    /** @type {{[x in 'currencyCodeA' | 'currencyCodeB' | 'date' | 'rateBuy' | 'rateSell']: number}[]} */
-    const currencies = await request('bank/currency');
-    return currencies.filter(v => v.currencyCodeA === USD && v.currencyCodeB === UAH)[0].rateSell;
+    try{
+        /** @type {{[x in 'currencyCodeA' | 'currencyCodeB' | 'date' | 'rateBuy' | 'rateSell']: number}[]} */
+        const currencies = await request('bank/currency');
+        return currencies.filter(v => v.currencyCodeA === USD && v.currencyCodeB === UAH)[0].rateSell;
+    } catch(e){
+        return defaultRate;
+    }
 };
